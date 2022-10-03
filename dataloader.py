e9c3eb53d049bce1810145b20df874304548bcc5
Mengyang Liu <mliu444@gatech.edu>
2022-10-03 17:00:44 -0400

    Init for resent baselines and dataloaders

--- dataloader.py
+++ dataloader.py
@@ -1,37 +1,130 @@
+import zipfile
+import os
+import numpy as np
+import pytorch_lightning as pl
 import torch
+from torch.utils.data import DataLoader, Dataset
+from PIL import Image
+from torchvision.transforms import ToTensor, Compose, Resize, Normalize, RandomHorizontalFlip, RandomVerticalFlip, RandomRotation
 
-class HDataset(torch.utils.data.Dataset):
-    def __init__(self, x, y):
-        self.x = x
-        self.y = y
-        self.x1 = x[self.y == 1]
-        self.x0 = x[self.y == 0]
-        self.y1 = y[self.y == 1]
-        self.y0 = y[self.y == 0]
+def unzip(filename):
+    with zipfile.ZipFile(filename, 'r') as zip_ref:
+        zip_ref.extractall("./")
+
+class XRayDataset(Dataset):
+    def __init__(self, stage='train', transform=None):
+        self.path = f'./chest_xray/{stage}'
+        self.stage = stage
+        self.transform = transform
+        self.data = []
+        self.labels = []
+        self.load_data()
+
+    def load_data(self):
+        data = []
+        labels = []
+        for i in os.listdir(self.path + "/NORMAL"):
+            if i.endswith('.jpeg'):
+                data.append(self.path + "/NORMAL/" + i)
+                labels.append(0)
+        if self.stage == 'train':
+            # Augument NORAML class with 2x data, since 1:3 ratio is not good
+            for i in range(2):
+                self.data.extend(data)
+                self.labels.extend(labels)
+        for i in os.listdir(self.path + "/PNEUMONIA"):
+            if i.endswith('.jpeg'):
+                self.data.append(self.path + "/PNEUMONIA/" + i)
+                self.labels.append(1)
+        
+
     def __len__(self):
-        return len(self.x)
+        return len(self.data)
 
     def __getitem__(self, idx):
-        return self.x[idx], self.y[idx]
+        if torch.is_tensor(idx):
+            idx = idx.tolist()
+        img = Image.open(self.data[idx])
+        img = img.convert("L")
+        if self.transform:
+            img = self.transform(img)
+            img /= 255
+        return img, self.labels[idx]
 
-class HDataLoader(object):
-    def __init__(self, dataset, batch_size, shuffle=True, num_workers=4):
-        self.dataset = dataset
-        self.batch_size = batch_size
-        self.shuffle = shuffle
-        self.num_workers = num_workers
+class XRaySampleDataset(Dataset):
+    def __init__(self, stage='train', sample_ratio=0.8, transform=None):
+        self.path = f'./chest_xray/{stage}'
+        self.transform = transform
+        self.sample_ratio = sample_ratio
+        self.data = []
+        self.labels = []
+        self.load_data()
 
-        self.data_loader = torch.utils.data.DataLoader(
-            dataset=self.dataset,
-            batch_size=self.batch_size,
-            shuffle=self.shuffle,
-            num_workers=self.num_workers,
-            pin_memory=True,
-        )
-
-    def __iter__(self):
-        for data in self.data_loader:
-            yield data
+    def load_data(self):
+        for i in os.listdir(self.path + "/NORMAL"):
+            if i.endswith('.jpeg'):
+                self.data.append(self.path + "/NORMAL/" + i)
+                self.labels.append(0)
+        for i in os.listdir(self.path + "/PNEUMONIA"):
+            if i.endswith('.jpeg'):
+                self.data.append(self.path + "/PNEUMONIA/" + i)
+                self.labels.append(1)
+        shuffle = np.random.shuffle(zip(self.data, self.labels))
+        self.data = [x for x, _ in shuffle]
+        self.labels = [y for _, y in shuffle]
+        self.data = self.data[:int(len(self.data) * self.sample_ratio)]
+        self.labels = self.labels[:int(len(self.labels) * self.sample_ratio)]
+        
 
     def __len__(self):
-        return len(self.data_loader)
+        return len(self.data)
+
+    def __getitem__(self, idx):
+        if torch.is_tensor(idx):
+            idx = idx.tolist()
+        img = Image.open(self.data[idx])
+        img = img.convert("L")
+        if self.transform:
+            img = self.transform(img)
+            img /= 255
+        return img, self.labels[idx]
+
+class XRayDataModule(pl.LightningDataModule):
+    RESIZE_SHAPE = [224, 224]
+
+    def __init__(self, batch_size=32, sample_ratio=None):
+        super().__init__()
+        self.batch_size = batch_size
+        self.sample_ratio = sample_ratio
+
+
+        transforms = Compose([
+            Resize(self.RESIZE_SHAPE),
+            RandomHorizontalFlip(),
+            RandomVerticalFlip(),
+            RandomRotation(15),
+            ToTensor(),
+        ])
+        if self.sample_ratio is None:
+            self.x_ray_train = XRayDataset(stage='train', transform=transforms)
+            self.x_ray_val = XRayDataset(stage='val', transform=transforms)
+            self.x_ray_test = XRayDataset(stage='test', transform=transforms)
+        else:
+            self.x_ray_train = XRaySampleDataset(stage='train', sample_ratio=self.sample_ratio, transform=transforms)
+            self.x_ray_val = XRaySampleDataset(stage='val', sample_ratio=self.sample_ratio, transform=transforms)
+            self.x_ray_test = XRaySampleDataset(stage='test', sample_ratio=self.sample_ratio, transform=transforms)
+
+    def setup(self, stage=None):
+        pass
+
+    def train_dataloader(self):
+        return DataLoader(self.x_ray_train, batch_size=self.batch_size, num_workers=4, shuffle=True)
+
+    def val_dataloader(self):
+        return DataLoader(self.x_ray_val, batch_size=self.batch_size, num_workers=4)
+
+    def test_dataloader(self):
+        return DataLoader(self.x_ray_test, batch_size=self.batch_size, num_workers=4)
+
+if __name__ == '__main__':
+    unzip('archive.zip')
--- main.py
+++ main.py
@@ -1,39 +1,21 @@
-import torch.nn as nn
-import torch
-from Backbone import Backbone
-from argparser import args
+import pytorch_lightning as pl
+from pytorch_lightning.strategies import DDPStrategy
+from dataloader import XRayDataModule
+from modules import BaselineModule
 
-DEVICE = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
-class Trainer(object):
-    def __init__(self, args):
-        self.args = args
-        self.model = Backbone(backbone=args.backbone, input_channel=args.input_channel, pretrained=args.pretrained).to(DEVICE)
-        self.loss = nn.BCELoss()
-        self.optimizer = torch.optim.Adam(self.model.parameters(), lr=self.args.lr)
-    
-    def train(self, train_loader, vali_loader):
-        print("Training")
-        for epoch in range(self.args.epochs):
-            print("Epoch: ", epoch)
-            for i, (x, y) in enumerate(train_loader):
-                out = self.model(x.to(DEVICE))
-                loss = self.loss(out, y.to(DEVICE))
-                self.optimizer.zero_grad()
-                loss.backward()
-                self.optimizer.step()
-            if i % 10 == 0:
-                self.eval(vali_loader)
-        torch.save(self.model.state_dict(), self.args.save_path)
-        return 0
-    
-    def eval(self, test_loader, return_pred=False):
-        self.model.eval()
-        print("Evaluating")
-        for i, (x, y) in enumerate(test_loader):
-            out = self.model(x.to(DEVICE))
-            loss = self.loss(out, y.to(DEVICE))
-        self.model.train()
-        return 0
-
-if __name__ == "__main__":
-    trainer = Trainer(args)
+models = ['resnet18']
+for model in models:
+    xd = XRayDataModule(64)
+    model = BaselineModule(model, 1)
+    ddp = DDPStrategy(process_group_backend="nccl", find_unused_parameters=False)
+    train_trainer = pl.Trainer(accelerator='gpu', 
+                                devices=6, 
+                                strategy=ddp, 
+                                max_epochs=50, 
+                                val_check_interval=0.5,
+                                reload_dataloaders_every_n_epochs=2,
+                                log_every_n_steps=5,
+                                auto_lr_find=True,
+                                )
+    train_trainer.fit(model=model, datamodule=xd)
+    train_trainer.test(model=model, datamodule=xd)
